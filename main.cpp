f65afad2bf24cf2fc3f657f264d6a0539943705d
darshdesai00 <darshdesai50@icloud.com>
2025-10-26 16:56:05 -0700

    Having some errors with read race, moving on to sorting race setup

--- main.cpp
+++ main.cpp
@@ -11,13 +11,17 @@ using namespace std;
 using namespace std::chrono;
 
 int main() {
-    // temporary test containers (have to use these later)
+    // test containrs for read timings 
     vector<string> vec;
     list<string> lst;
     set<string> st;
 
     cout << "*** DATA STRUCTURES RACES ***" << endl;
 
+     cout << left << setw(12) << "Operation"
+         << setw(12) << "Vector"
+         << setw(12) << "List"
+         << setw(12) << "Set" << endl;
 
     // Read Race
     string code;
@@ -26,7 +30,7 @@ int main() {
     auto end = high_resolution_clock::now();
 
     // Vector Read
-    fin.open("codes.txt")
+    fin.open("codes.txt");
     start = high_resolution_clock::now();
     while (fin >> code) {
         vec.push_back(code);
@@ -36,7 +40,7 @@ int main() {
     fin.close();
 
     // List Read
-    fin.open("codes.txt")
+    fin.open("codes.txt");
     start = high_resolution_clock::now();
     while (fin >> code) {
         lst.push_back(code);
@@ -49,12 +53,21 @@ int main() {
     fin.open("codes.txt");
     start = high_resolution_clock::now();
     while (fin >> code) {
+        st.insert(code);
     }
     end = high_resolution_clock::now();
     auto vec_read = duration_cast<microseconds>(end - start).count();
-    fin.close()
+    fin.close();
+
+    // Display timing results
+    cout << left << setw(12) << "Read"
+         << setw(12) << vec_read
+         << setw(12) << lst_read
+         << setw(12) << set_read << endl;
 
 
-    
+    cout << endl << "Success" << endl;
 
+    return 0;
 
+}
