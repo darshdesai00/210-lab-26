86bc2c62b13c1e34ddee69ef2e198e517d1f15fd
darshdesai00 <darshdesai50@icloud.com>
2025-10-26 16:16:37 -0700

    Added the includes, now just trying to see if the chrono timing will work

--- main.cpp
+++ main.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
 #include <chrono>
+#include <iomanip>
+#include <string>
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-    
+    // temporary test containers (have to use these later)
+    vector<string> vec;
+    list<string> lst;
+    set<string> st;
+
+    cout << "*** DATA STRUCTURES RACES ***" << endl;
+
+    // testing out the chrono timing test
+    auto start = high_resolution_clock::now();
+
+    // to test stopwatch
+    for (int i = 0; i < 100000; ++i) {
+        vec.push_back(to_string(i));
+    }
+    // ends the timing 
+    auto end = high_resolution_clock::now();
+    // sees how long the duration is
+    auto duration = duration_cast<microseconds>(end - start);
+
+    // output time in microseconds 
+    cout << "Example chrono check: " << duration.count()
+         << " microseconds" << endl;
 
     return 0;
 }
-
-/* syntax examples:
-auto start = high_resolution_clock::now()
-auto end = high_resolution_clock::now()
-auto duration = duration_cast<milliseconds>(end - start)
-duration.count() references elapsed milliseconds
-*/
